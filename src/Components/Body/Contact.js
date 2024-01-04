17170e0ce13ce80b4bdca617e2a9e3db82fe716f
siddhesh494 <siddhesh.ss26@gmail.com>
2024-01-04 23:10:51 +0530

    some css style changes

--- src/Components/Body/Contact.js
+++ src/Components/Body/Contact.js
@@ -5,14 +5,14 @@ import { EMAIL_LINK, LINKEDLN_LINK } from '../../Common/Constant'
 const Contact = () => {
 
   return (
-    <div className="my-28">
+    <div className="my-28 mx-10">
       <div className="flex flex-col text-center">
         <span className="text-md text-slate-800">Get In Touch</span>
         <span className="text-4xl">Contact Me</span>
       </div>
 
       <div className="flex justify-center mt-5">
-        <div className="flex gap-10 min-w-60 border border-black px-14 py-5 rounded-xl">
+        <div className="flex flex-wrap gap-10 min-w-60 border border-black px-14 py-5 rounded-xl">
           <div 
             className='flex gap-1 cursor-pointer hover:underline'
             onClick={() => {
--- src/Components/Body/Experience.js
+++ src/Components/Body/Experience.js
@@ -10,9 +10,9 @@ const Experience = () => {
         <span className="text-4xl">Experience</span>
       </div>
 
-      <div className="flex flex-col gap-10 justify-center mt-5 items-center  md:flex-row md:items-start">
+      <div className="flex flex-col gap-10 justify-center mt-5 items-center  md:flex-row md:items-start mx-10">
         
-        <div className="border border-black rounded-3xl w-96 p-5 text-center">
+        <div className="border border-black rounded-3xl min-w-80 p-5 text-center md:min-w-96">
           <span className="text-xl font-bold">Web Technologies</span>
           <div className="grid justify-items-center grid-cols-12 mt-5 gap-y-5 ">
             {map(EXPERIENCE.Web_Technologies, (item, index) => {
@@ -27,7 +27,7 @@ const Experience = () => {
           </div>
         </div>
         
-        <div className="border border-black rounded-3xl w-96 p-5 text-center">
+        <div className="border border-black rounded-3xl min-w-80 p-5 text-center md:min-w-96">
           <span className="text-xl font-bold">Other Concepts</span>
           <div className="grid justify-items-center grid-cols-12 mt-5 gap-y-5 ">
             {map(EXPERIENCE.others, (item, index) => {
